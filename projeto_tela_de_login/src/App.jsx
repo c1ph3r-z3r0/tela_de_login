53b5ab3227b6d1eed7dc60a41d1a72dc3be5e89f
Wescley Garrett <145073395+c1ph3r-z3r0@users.noreply.github.com>
2025-01-12 21:49:34 +0000

    Firt commit new file:   package-lock.json new file:   projeto_tela_de_login/.gitignore new file:   projeto_tela_de_login/README.md new file:   projeto_tela_de_login/eslint.config.js new file:   projeto_tela_de_login/index.html new file:   projeto_tela_de_login/package-lock.json new file:   projeto_tela_de_login/package.json new file:   projeto_tela_de_login/public/vite.svg new file:   projeto_tela_de_login/src/App.css new file:   projeto_tela_de_login/src/App.jsx new file:   projeto_tela_de_login/src/Components/Login/Login.css new file:   projeto_tela_de_login/src/Components/Login/Login.jsx new file:   projeto_tela_de_login/src/assets/plano_de_fundo.webp new file:   projeto_tela_de_login/src/assets/react.svg new file:   projeto_tela_de_login/src/index.css new file:   projeto_tela_de_login/src/main.jsx new file:   projeto_tela_de_login/vite.config.js

---
+++ projeto_tela_de_login/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Login from './Components/Login/Login'
+
+function App() {
+
+  return (
+    <div className="App">
+      <p>Página de Login</p>
+      <Login />
+    </div>
+  )
+}
+
+export default App
